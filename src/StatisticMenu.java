2c036e6e65c8930c282ca51764515c6d4184e39f
Huy Vu <67470840+wyb-lv@users.noreply.github.com>
2024-12-22 23:03:52 +0700

    Update StatisticMenu.java

--- src/StatisticMenu.java
+++ src/StatisticMenu.java
@@ -1,39 +1,34 @@
 import java.util.Scanner;
 
-public class Menu {
+public class StatisticMenu {
     private Scanner scanner;
-    public Menu(){
+    public StatisticMenu(){
         this.scanner = new Scanner(System.in);
     }
     protected void run(){
         StatisticFile file = new StatisticFile();
+        statisticManagement management = new statisticManagement(file, scanner);
         while (true) {
-            System.out.println("\nMenu:");
+            System.out.println("==== Statistic Management ====");
             System.out.println("1. Passenger Statistic");
             System.out.println("2. Member Statistic");
             System.out.println("3. Flight Statistic");
             System.out.println("4. Exit");
-
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
-            scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    statisticManagement management = new statisticManagement();
                     management.printPassengerStatistics();
                     break;
                 case 2:
-                    statisticManagement management1 = new statisticManagement();
-                    management1.printMemberStatistic();
+                    management.printMemberStatistic();
                     break;
                 case 3:
-                    statisticManagement management2 = new statisticManagement();
-                    management2.printFlightStatistic();
+                    management.printFlightStatistic();
                     break;
                 case 4:
                     System.out.println("Exiting the program...");
-                    scanner.close();
                     return;
                 default:
                     System.out.println("Invalid choice! Please select a valid option.");
